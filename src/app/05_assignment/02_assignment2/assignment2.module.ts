5a54665b4e2ed936704ba27246b6d108b69b97f2
Deepshikha Tyagi <deepshikha.tyagi@nagarro.com>
2020-04-21 20:33:51 +0530

    Assignment 2 completed

--- src/app/05_assignment/02_assignment2/assignment2.module.ts
+++ src/app/05_assignment/02_assignment2/assignment2.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Assignment2Component } from './containers/assignment2.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
+  imports : [
+    CommonModule
+  ],
   declarations: [
     Assignment2Component
   ],
--- src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
+++ src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AssignmentComponent, Todo } from '../../assignment.component';
 
 @Component({
   selector: 'app-assignment2',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class Assignment2Component {
 
+  todoList : Todo[]
+  constructor(private assignComp : AssignmentComponent){}
+  
+  ngOnInit(){
+    this.todoList = this.assignComp.todos
+    console.log(this.todoList)
+  }
+
+  AddClass(event){
+    event.target.parentNode.className = "active"
+  }
+
+  CreateTableInput(input : string) : HTMLTableCellElement{
+    let cell = document.createElement('td')
+    cell.textContent = input
+    return cell
+  }
 }
