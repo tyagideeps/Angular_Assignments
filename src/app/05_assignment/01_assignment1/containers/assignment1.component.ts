595c385dcdbe61c281fcf9a7a038306c35b324b4
Deepshikha Tyagi <deepshikha.tyagi@nagarro.com>
2020-04-21 15:49:17 +0530

    Assignment 1 completed

--- src/app/05_assignment/01_assignment1/containers/assignment1.component.ts
+++ src/app/05_assignment/01_assignment1/containers/assignment1.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./assignment1.component.css']
 })
 export class Assignment1Component {
-
+  inputTxt = " "
+  ShowInputText(inputData:any){
+    this.inputTxt = inputData.value
+  }
 }
